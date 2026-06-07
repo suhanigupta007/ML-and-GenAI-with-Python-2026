c7bac1be9735113dd3caffaa796aaffa107a7e80
Deepanshi Nagar <deepanshi8586@gmail.com>
2026-06-07 14:52:31 +0530

    assignment_1.py

--- Assignment_1/DeepanshiNagar_03101032024/assignment_1.py
+++ Assignment_1/DeepanshiNagar_03101032024/assignment_1.py
@@ -1,33 +1,34 @@
+#1
 len = float(input("enter length:"))
 breadth = float(input("enter breadth:"))
 area = len*breadth
 print("area =", area)
 
-
+#2
 p = float(input("enter principal:"))
 r = float(input("enter Rate:"))
 t = float(input("enter Time:"))
 si = (p*r*t)/100
 print("simple interest:", si)
 
-
+#3
 c = float(input("Enter Temperature in Celsius: "))
 f = (c * 9/5) + 32
 print("Temperature in Fahrenheit =", f)
 
-
+#4
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 c = float(input("Enter third number: "))
 avg = (a + b + c) / 3
 print("Average =", avg)
 
-
+#5
 n = int(input("Enter a number: "))
 print("Square =", n ** 2)
 print("Cube =", n ** 3)
 
-
+#6
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 a, b = b, a
@@ -35,7 +36,7 @@ print("After Swapping:")
 print("a =", a)
 print("b =", b)
 
-
+#7
 name = input("Enter Student Name: ")
 roll_no = input("Enter Roll Number: ")
 m1 = float(input("Enter marks of Subject 1: "))
